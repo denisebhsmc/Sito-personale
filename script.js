b4a65ab15c9d5b07539da787f47db442d93970f3
denisebhsmc <153520794+denisebhsmc@users.noreply.github.com>
2024-07-04 22:01:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,28 @@ window.addEventListener('click', function(event) {
 });
 
 
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(n) {
+    showSlide(currentSlide + n);
+}
+
+// Inizializza lo slider
+showSlide(currentSlide);
+
 
 
 
